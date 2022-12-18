104e643c9e328f94a375abe5f523bc0efa751fae
Elis404 <73383632+Elis404@users.noreply.github.com>
2022-12-19 02:27:46 +0300

    Added subcases

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -4,54 +4,54 @@
 #include "../calculator.cpp"
 
 TEST_CASE("addition testing") {
-    CHECK(addition(1, 1) == 2);
-    CHECK(addition(-1, 1) == 0);
-    CHECK(addition(1, -1) == 0);
-    CHECK(addition(-1, -1) == -2);
+    SUBCASE("smth") CHECK(addition(1, 1) == 2);
+    SUBCASE("smth") CHECK(addition(-1, 1) == 0);
+    SUBCASE("smth") CHECK(addition(1, -1) == 0);
+    SUBCASE("smth") CHECK(addition(-1, -1) == -2);
 }
 
 TEST_CASE("subtraction testing") {
-    CHECK(subtraction(1, 1) == 0);
-    CHECK(subtraction(-1, 1) == -2);
-    CHECK(subtraction(1, -1) == 2);
-    CHECK(subtraction(-1, -1) == 0);
+    SUBCASE("smth") CHECK(subtraction(1, 1) == 0);
+    SUBCASE("smth") CHECK(subtraction(-1, 1) == -2);
+    SUBCASE("smth") CHECK(subtraction(1, -1) == 2);
+    SUBCASE("smth") CHECK(subtraction(-1, -1) == 0);
 }
 
 TEST_CASE("multiplication testing") {
-    CHECK(multiplication(3, 3) == 9);
-    CHECK(multiplication(-3, 3) == -9);
-    CHECK(multiplication(3, -3) == -9);
-    CHECK(multiplication(-3, -3) == 9);
+    SUBCASE("smth") CHECK(multiplication(3, 3) == 9);
+    SUBCASE("smth") CHECK(multiplication(-3, 3) == -9);
+    SUBCASE("smth") CHECK(multiplication(3, -3) == -9);
+    SUBCASE("smth") CHECK(multiplication(-3, -3) == 9);
 }
 
 TEST_CASE("division testing") {
-    CHECK(division(3, 3) == 1);
-    CHECK(division(-3, 3) == -1);
-    CHECK(division(3, -3) == -1);
-    CHECK(division(-3, -3) == 1);
+    SUBCASE("smth") CHECK(division(3, 3) == 1);
+    SUBCASE("smth") CHECK(division(-3, 3) == -1);
+    SUBCASE("smth") CHECK(division(3, -3) == -1);
+    SUBCASE("smth") CHECK(division(-3, -3) == 1);
 }
 
 TEST_CASE("percent testing") {
-    CHECK(percent_of(1) == 0.01);
-    CHECK(percent_of(50) == 0.5);
-    CHECK(percent_of(75) == 0.75);
-    CHECK(percent_of(100) == 1);
+    SUBCASE("smth") CHECK(percent_of(1) == 0.01);
+    SUBCASE("smth") CHECK(percent_of(50) == 0.5);
+    SUBCASE("smth") CHECK(percent_of(75) == 0.75);
+    SUBCASE("smth") CHECK(percent_of(100) == 1);
 }
 
 TEST_CASE("pow testing") {
-    CHECK(pow(1, 0) == 1);
-    CHECK(pow(1, 3) == 1);
-    CHECK(pow(2, 0) == 1);
-    CHECK(pow(2, 2) == 4);
-    CHECK(pow(2, 10) == 1024);
+    SUBCASE("smth") CHECK(pow(1, 0) == 1);
+    SUBCASE("smth") CHECK(pow(1, 3) == 1);
+    SUBCASE("smth") CHECK(pow(2, 0) == 1);
+    SUBCASE("smth") CHECK(pow(2, 2) == 4);
+    SUBCASE("smth") CHECK(pow(2, 10) == 1024);
 }
 
 TEST_CASE("factorial testing") {
-    CHECK(factorial(1) == 1);
-    CHECK(factorial(2) == 2);
-    CHECK(factorial(3) == 6);
-    CHECK(factorial(5) == 120);
-    CHECK(factorial(8) == 40320);
+    SUBCASE("smth") CHECK(factorial(1) == 1);
+    SUBCASE("smth") CHECK(factorial(2) == 2);
+    SUBCASE("smth") CHECK(factorial(3) == 6);
+    SUBCASE("smth") CHECK(factorial(5) == 120);
+    SUBCASE("smth") CHECK(factorial(8) == 40320);
 }
 
 
