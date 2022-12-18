9c201768de21c3f46947fca03b7457172bd40beb
Elisey <eliseyta2004@gmail.com>
2022-12-19 01:37:52 +0300

    Upload all

---
+++ calculator.h
@@ -0,0 +1,17 @@
+#pragma once
+
+double addition(double a, double b);
+
+double subtraction(double a, double b);
+
+double multiplication(double a, double b);
+
+double division(double a, double b);
+
+double percent_of(double a);
+
+double pow(double a, double n);
+
+double factorial(double a);
+
+
